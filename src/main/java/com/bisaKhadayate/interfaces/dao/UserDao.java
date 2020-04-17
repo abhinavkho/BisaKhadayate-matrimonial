a3be9ee4e791d4e9c2104d5a953d4bacfc28285a
ab9khoti <ab9khoti@gmail.com>
2020-04-17 17:51:42 +0530

    age filter

--- src/main/java/com/bisaKhadayate/interfaces/dao/UserDao.java
+++ src/main/java/com/bisaKhadayate/interfaces/dao/UserDao.java
@@ -28,7 +28,7 @@ public interface UserDao extends CrudRepository<User,Integer>  {
 			+ " (1=CASE  WHEN ?3='' THEN 1 ELSE u.firstName=?3  END )"
 			+ " and (1=CASE  WHEN ?4='' THEN 1 ELSE u.lastName=?4  END )"
 			+ " and (1=CASE  WHEN ?5='' THEN 1 ELSE u.caste=?5  END )"
-			+ " and (1=CASE  WHEN ?6='' THEN 1 ELSE u.gotra=?6  END ) and (1=CASE WHEN ?7=0 THEN 1 ELSE floor(datediff(CURDATE(),CONVERT(u.dob, DATE))/365)<=?7 END )limit ?8,10", nativeQuery = true)
+			+ " and (1=CASE  WHEN ?6='' THEN 1 ELSE u.gotra=?6  END ) and (1=CASE WHEN ?7=0 THEN 1 ELSE floor(datediff(CURDATE(),CONVERT(u.dob, DATE))/365)>=?7 END )limit ?8,10", nativeQuery = true)
 	List<Map<String,Object>> searchFilterResult(Character gender,boolean isActive , String firstName , String lastName ,String caste , String gotra ,int age,int index );
 
 	
@@ -39,7 +39,7 @@ public interface UserDao extends CrudRepository<User,Integer>  {
 			+ " (1=CASE  WHEN ?3='' THEN 1 ELSE u.firstName=?3  END )"
 			+ " and (1=CASE  WHEN ?4='' THEN 1 ELSE u.lastName=?4  END )"
 			+ " and (1=CASE  WHEN ?5='' THEN 1 ELSE u.caste=?5  END )"
-			+ " and (1=CASE  WHEN ?6='' THEN 1 ELSE u.gotra=?6  END ) and (1=CASE WHEN ?7=0 THEN 1 ELSE floor(datediff(CURDATE(),CONVERT(u.dob, DATE))/365)<=?7 END )", nativeQuery = true)
+			+ " and (1=CASE  WHEN ?6='' THEN 1 ELSE u.gotra=?6  END ) and (1=CASE WHEN ?7=0 THEN 1 ELSE floor(datediff(CURDATE(),CONVERT(u.dob, DATE))/365)>=?7 END )", nativeQuery = true)
 	Integer searchFilterResultCount(Character gender,boolean isActive , String firstName , String lastName ,String caste , String gotra  ,int age);
 
 
